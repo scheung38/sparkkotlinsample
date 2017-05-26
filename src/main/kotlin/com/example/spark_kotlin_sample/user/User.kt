ebd87bfa2cfa0977706d64caacfb8dce7162cf74
Sebastian Cheung <sebastian_cheung@yahoo.com>
2017-05-26 22:22:16 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/spark_kotlin_sample/user/User.kt
@@ -0,0 +1,3 @@
+package com.example.spark_kotlin_sample.user
+
+data class User(val name: String, val email: String, val id: Int)
---
+++ src/test/kotlin/com/example/spark_kotlin_sample/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.example.spark_kotlin_sample
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
